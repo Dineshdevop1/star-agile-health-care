f95252dd561dff4db754a5e209ea3e8aae1b0f86
Dineshdevop1 <88160735+Dineshdevop1@users.noreply.github.com>
2023-05-09 23:00:19 +0530

    Update main.tf

--- serverfiles/main.tf
+++ serverfiles/main.tf
@@ -1,12 +1,12 @@
 resource "aws_instance" "test-server" {
   ami           = "ami-02eb7a4783e7e9317" 
   instance_type = "t2.medium" 
-  key_name = "kittu-key"
-  vpc_security_group_ids= ["sg-0025765b8d7b2983e"]
+  key_name = "project"
+  vpc_security_group_ids= ["sg-001ed457cb6d9e6b1"]
   connection {
     type     = "ssh"
     user     = "ubuntu"
-    private_key = file("./kittu-key.pem")
+    private_key = file("project.pem")
     host     = self.public_ip
   }
   provisioner "remote-exec" {
@@ -29,7 +29,7 @@ resource "aws_instance" "test-server" {
              # "sudo microk8s status --wait-ready",
              # "sudo microk8s enable dns ingress",
               "sudo microk8s status",
-              "sudo microk8s kubectl create deployment health-deploy --image=aravindrevalli59/healthcare:latest",
+              "sudo microk8s kubectl create deployment health-deploy --image=dineshk1996/healthcare:latest",
               "sudo microk8s kubectl expose deployment health-deploy --port=8082 --type=NodePort",
               "sudo microk8s kubectl get svc",
               "sudo echo Public IP Address of the Instance",
